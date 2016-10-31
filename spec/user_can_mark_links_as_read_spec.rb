a91e01b0f903a0e1807762fa610e83ea9094e171
Zack Forbing <zackforbing@gmail.com>
2016-10-31 14:08:23 -0600

    add factory girl

--- spec/user_can_mark_links_as_read_spec.rb
+++ spec/user_can_mark_links_as_read_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature 'User can mark Link as read' do
   scenario 'user sees his links and marks one as read' do
-    user = User.create!(email: 'andrew@turing.io', password: 'test', password_confirmation: 'test')
+    user = create(:user)
     page.set_rack_session(user: user.id)
 
   end
--- spec/user_can_submit_link_spec.rb
+++ spec/user_can_submit_link_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.feature 'User can submit a Link' do
   scenario 'on visiting root they are prompted to sign up or login and can do either' do
-    user = User.create!(email: 'andrew@turing.io', password: 'test', password_confirmation: 'test')
+    user = create(:user)
+    create(:link, 3)
     page.set_rack_session(user_id: user.id)
 
     visit root_path
@@ -12,8 +13,8 @@ RSpec.feature 'User can submit a Link' do
     click_on 'Add Link'
 
     expect(current_path).to eq(root_path)
-    # expect(page).to have_content('test')
-    # expect(page).to have_link('http://www.google.com', href: 'http://www.google.com')
-    # expect(page).to have_content('false')
+    expect(page).to have_content('test')
+    expect(page).to have_link('http://www.google.com', href: 'http://www.google.com')
+    expect(page).to have_content('false')
   end
 end
