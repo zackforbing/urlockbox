c7717d0ef6e39a702dc377d42b4a0694dd109de5
Zack Forbing <zackforbing@gmail.com>
2016-11-08 11:06:46 -0700

    build ajax call for update, rebuild link index for css

--- app/assets/javascripts/handleLinks.js
+++ app/assets/javascripts/handleLinks.js
@@ -4,5 +4,17 @@ $(document).ready(() => {
 
 function toggleReadStatus() {
   e.preventDefault();
-  
+  let $link = $(this).closest("tr");
+  let linkParams = 
+
+  $.ajax( {
+    url: "api/v1/links/" + $link.data("id"),
+    data: linkParams,
+    type: "put"
+  })
+  .fail(handleError);
+}
+
+function handleError(error) {
+  console.log(error);
 }
