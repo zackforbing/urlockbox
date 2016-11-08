f7ed0b340b7add98b363be6fe728edbb66773a5e
Zack Forbing <zackforbing@gmail.com>
2016-11-08 09:19:35 -0700

    first commit of the day - rip out unnecesary files fix update method to validate url remove extra require in Link model remove redundancy in User model with respect to password confirmation update title in application.html.erb fix indentation problems put feature tests in features folder

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
   has_many :links
   validates :email, presence: true, uniqueness: true
-  validates :password, confirmation: true
-  validates :password_confirmation, presence: true
   has_secure_password
 end
